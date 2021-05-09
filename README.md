ae0ca09dec8f6a7e151a8cc1e41c5c0ab411bd30
henryiscoder <68643463+henryiscoder@users.noreply.github.com>
2021-05-09 21:38:15 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# nodemailer-example
-Simple Nodemailer App for Beginners
+# Nodemailer App for Beginners
+
+  
+
+Nodemailer is a node.js module to interact with the SMTP and send e-mails. Here the simple app for understand nodemailer module. Use it on your own.
+
+## Disclaimer
+
+Nodemailer module is not belongs to me. It is just simple app for beginners.
+
+## Installation
+* ```git clone link```
+* ```npm install```
+* ```node server.js```
+
+* Go to the [localhost](http://localhost:8080)
+
+## Links 
+* [Nodemailer Docs](https://nodemailer.com/about/)
+* [Nodemailer Github Repository](https://github.com/nodemailer/nodemailer/)
+* [Support Nodemailer](https://www.paypal.com/donate)
---
+++ server.js
@@ -0,0 +1,46 @@
+const nodemailer = require("nodemailer");
+const express = require("express")
+const dotenv = require("dotenv")
+const app = express()
+
+app.use(express.static("public"))
+app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }));
+
+dotenv.config()
+
+const transporter =  nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // use SSL
+  auth: {
+      user: process.env.USER,
+      pass: process.env.PASS
+  }
+});
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.post("/send", (req, res) => {
+  const email = req.body.email
+  var message = {
+    from: process.env.USER,
+    to: email,
+    subject: "Success",
+    html: "<p><strong>Successfully sent the email! Now you understand the SMTP and mail services. If you love it, please rate the <a href = \"link\">project</a></strong></p>"
+  };
+
+  transporter.sendMail(message, function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+    }
+  });
+
+  res.redirect("back")
+})
+
+app.listen(8080, () => console.log("Server started.."))
